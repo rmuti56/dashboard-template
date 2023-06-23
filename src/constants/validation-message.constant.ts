1020ce389a638f79121e7cd6d15141993cfbcafe
chokwatcharathep <chok.watcharathep@maqe.com>
2023-06-23 23:54:54 +0700

    refactor(locale): move config to the same file

--- src/constants/validation-message.constant.ts
+++ src/constants/validation-message.constant.ts
@@ -1,5 +1,5 @@
 import { validationMessages } from "@/intl/validation-message.intl";
-import { intl } from "@/utils/intl.util";
+import { intl } from "@/libs/intl";
 
 export const VALIDATION_MESSAGES = Object.freeze({
   REQUIRED: intl.$t(validationMessages.required),
--- src/store/slices/localization.slice.ts
+++ src/store/slices/localization.slice.ts
@@ -2,16 +2,12 @@ import { Locale } from "@/enums/locale.enum";
 import { StorageKey } from "@/enums/storage-key.enum";
 import { PayloadAction, createSlice } from "@reduxjs/toolkit";
 
-type Messages = undefined | Record<string, string>;
-
 export type LocalizationState = {
   locale: Locale;
-  messages: Messages;
 };
 
 const initialState: LocalizationState = {
   locale: localStorage.getItem(StorageKey.LOCALE) as Locale || Locale.EN, // Default locale
-  messages: undefined,
 };
 
 const localizationSlice = createSlice({
@@ -21,12 +17,9 @@ const localizationSlice = createSlice({
     setLocale: (state, action: PayloadAction<Locale>) => {
       state.locale = action.payload;
     },
-    setMessages: (state, action: PayloadAction<Messages>) => {
-      state.messages = action.payload;
-    },
   },
 });
 
-export const { setLocale, setMessages } = localizationSlice.actions;
+export const { setLocale } = localizationSlice.actions;
 
 export default localizationSlice.reducer;
